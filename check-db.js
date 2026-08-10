529eb0eab0f598410e223f11c58467a00cd3ceaa
Rushish Mewada <rushish.mewada@asmitagroup.com>
2026-08-10 11:20:33 +0530

    Cleanup temp db test files

--- check-db.js
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.invoiceTemplate.findMany().then(res => {
-  console.log(JSON.stringify(res, null, 2));
-}).catch(console.error).finally(() => prisma.$disconnect());
