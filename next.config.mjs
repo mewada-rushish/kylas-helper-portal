3e4412770393f5251d561854f8865f9bef7fdf63
Rushish Mewada <rushish.mewada@asmitagroup.com>
2026-08-17 12:01:29 +0530

    fix: force Next.js standalone tracer to include @sparticuz/chromium bin directory using outputFileTracingIncludes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: false,
   allowedDevOrigins: ['192.168.0.87'],
   serverExternalPackages: ['@sparticuz/chromium'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./node_modules/@sparticuz/chromium/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
